45b82b76475d97a5e6f0ec7a8323cc8b3dbc6967
naavapeledgie <naavajacqueline@gmail.com>
2026-04-17 12:00:16 +0300

    removed a model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Customer, Sale
+from .models import Product, Customer, Sale, SaleItem
 
 admin.site.register(Product)
 admin.site.register(Customer)
