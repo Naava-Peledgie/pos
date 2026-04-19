b14a0cb09398ced208456c74f1238710ed4c0171
naavapeledgie <naavajacqueline@gmail.com>
2026-04-19 10:46:13 +0300

    corrected atrribute error

--- pos_project/urls.py
+++ pos_project/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.admin_site.urls),
+    path('admin/', admin.site.urls),
     path('', include('core.urls')), # This connects your core app
 ]
