75688e75c85f603310d94e0068bd7109047a9561
naavapeledgie <naavajacqueline@gmail.com>
2026-04-23 00:02:45 +0300

    addes new colums list

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Product, Customer, Sale, SaleItem
 
-admin.site.register(Product)
+# 1. Advanced Registration for Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Adds columns for price and stock to the list view 
+    list_display = ('name', 'category', 'price', 'stock_quantity', 'created_at')
+    
+    # Adds a search bar for finding products quickly 
+    search_fields = ('name', 'category')
+    
+    # Adds a sidebar filter 
+    list_filter = ('category',)
+
+# 2. Basic Registration for other models
 admin.site.register(Customer)
 admin.site.register(Sale)
 admin.site.register(SaleItem)
